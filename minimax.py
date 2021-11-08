7886a7c721f5933a792a61599bdfa899e9082553
JonathanWilkins1 <52672297+JonathanWilkins1@users.noreply.github.com>
2021-11-08 12:52:14 -0500

    Add files via upload

--- minimax.py
+++ minimax.py
@@ -1,5 +1,6 @@
 ### File: minimax.py
 ### Classes defined: MinimaxPlayer
+### Authors: Jonathan Wilkins and Simon Schoelkopf
 
 from konane import *
 
@@ -17,12 +18,15 @@ class MinimaxPlayer(Konane, Player):
         moves = self.generateMoves(board, self.side)
         if len(moves) == 0:
             return []
-        value = self.minimax(board, self.limit, True)
-        print(value)
+        value = -float("inf")
+        bestMove = []
         for move in moves:
-            if value == self.eval(self.nextBoard(board, self.side, move)):
-                return move
-        return []
+            temp = self.minimax(self.nextBoard(board, self.side, move),
+                self.limit - 2, False)
+            if (temp > value):
+                value = temp
+                bestMove = move
+        return bestMove
     def minimax(self, board, depth, isMax):
         if depth == 0:
             return self.eval(board)
@@ -34,28 +38,37 @@ class MinimaxPlayer(Konane, Player):
         value = -float("inf")
         moves = self.generateMoves(board, self.side)
         if (len(moves) == 0):
-            return float("inf")
+            return self.eval(board)
         for move in moves:
-            value = max(self.minimax(self.nextBoard(board, self.opponent(self.side), move),
+            value = max(self.minimax(self.nextBoard(board, self.side, move),
                         depth - 1, False), value)
         return value
     def minimize(self, board, depth):
         value = float("inf")
         moves = self.generateMoves(board, self.opponent(self.side))
         if (len(moves) == 0):
-            return -float("inf")
+            return self.eval(board)
         for move in moves:
-            value = min(self.minimax(self.nextBoard(board, self.side, move),
+            value = min(self.minimax(self.nextBoard(board, self.opponent(self.side), move),
                         depth - 1, True), value)
         return value
     def eval(self, board):
-        return len(self.generateMoves(board, self.side))
-
-    ############# looking too far, take a step back and stop recursing before stepping down
-    #############   into a terminal state and getting inf or -inf for len(generateMoves)
-
+        value = len(self.generateMoves(board, self.side))
+        for r in range(len(board)):
+            if (board[r][0] == self.side):
+                value += 1
+        for r in range(len(board)):
+            if (board[r][len(board) - 1] == self.side):
+                value += 1
+        for c in range(len(board)):
+            if (board[0][c] == self.side):
+                value += 1
+        for c in range(len(board)):
+            if (board[len(board) - 1][c] == self.side):
+                value += 1
+        return value
 
 # Need these lines in this file in order to test the MinimaxPlayer class
 # Comment out these lines in the konane.py file so it's not run twice
-game = Konane(8)
-game.playNGames(1, MinimaxPlayer(8, 2), SimplePlayer(8), 1)
+# game = Konane(8)
+# game.playNGames(1, MinimaxPlayer(8, 2), SimplePlayer(8), 1)
